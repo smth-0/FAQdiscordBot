649e36cb6fdcc62fe41a05374edd2cc4113c4b18
smth-0 <someth183@gmail.com>
2020-02-16 05:23:48 +0000

    early core changes

--- main.py
+++ main.py
@@ -4,29 +4,33 @@ from lists import *
 import discord
 from discord.ext import commands
 
-client = commands.Bot(command_prefix='.')
 
+client = commands.Bot(command_prefix='.')
 
 @client.event
 async def on_ready():
     print("on")
 
-
+#how the person brings up the message
 @client.command(aliases=['quote'])
 async def quotes_safezone(ctx):
-    await ctx.send(quotes.get("safezone"))
-
-
-@client.command(aliases=['quotes'])
-async def open_book(ctx):
-    # we have to define book
-    await ctx.send("book")
-
+    #needs to be turned into a book
+    msg = await ctx.send("Book Example")
 
 @client.event
-async def on_reaction_add(reaction, ctx):
-    if reaction.emoji == '👍':
-        await ctx.send("book")
+async def test(message):
+  if message.author.bot:
+    print("something")
+    await client.delete_message(message)
+
+@client.event
+async def on_reaction_add(reaction, channel):
+  if reaction.emoji == '👍':
+    await channel.send("Book Example") 
+
+@client.command(aliases=['next'])
+async def next_page(ctx):
+  await ctx.message.edit(content="new page")
 
 
 client.run(key)
--- settings.py
+++ settings.py
@@ -1,3 +1,7 @@
+import discord
+from discord.ext import commands
+
+msg = ""
 lists = 0
 quotelist = 0
-key = 'Njc4MDI5MzM3OTk1Mzc4Njg4.Xkc2GQ.M4gPOj0g5vfO_DPwayArB-eesDY'
+key = 'Njc4MDI5MzM3OTk1Mzc4Njg4.XkicpA.r7dhmK_WG1U2m7EwYYit5jfUBio'
