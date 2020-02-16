92b2ed3977d0288fb3f7025ba949367a0b61a2ab
smth-0 <someth183@gmail.com>
2020-02-16 15:21:15 +0000

    minor changes

--- settings.py
+++ settings.py
@@ -4,5 +4,5 @@ from discord.ext import commands
 msg = ""
 lists = 0
 quotelist = 0
-key = 'Njc4MDI5MzM3OTk1Mzc4Njg4.XklKqw.uF4YJvEDByL1QdrD21ALNnXmpXg'  
+key = 'Njc4MDI5MzM3OTk1Mzc4Njg4.XklZXA.1ExI02ThXXNTDpOQVbeC-zp6i6Q'  
 #now its sweers :dab:
