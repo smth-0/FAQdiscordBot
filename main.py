011ae175be4599def9a1b90c97aec0cf5f164d30
smth <someth183@gmail.com>
2020-02-17 19:02:16 +0600

    main features update

--- main.py
+++ main.py
@@ -8,20 +8,49 @@ from threading import Thread
 client = commands.Bot(command_prefix='.', case_insensitive=True)
 
 current_books = {}
+last_100_books = []
+left_turn, right_turn = '⬅️', '➡️'
+
+FBdev = 'Frozenbyte Developer'  # quotable person
+quotemod = 'Frozenbyte Developer'  # role allowed to put quotes into DB
+book_timeout = 500  # seconds after last flip before it deletes the book -- doesnt works, ignore --
 
 
 class Book:
-    def __init__(self, ctx):
-        print('new book created', ctx.message.channel.name)
-        self.page = 0
+    def __init__(self, ctx, tags=None):
+        if len(tags) == 0:
+            tags = None
+        print('new book created in #', ctx.message.channel.name, 'with tags:', tags)
+        self.page = 1
+        self.results = wrapper.fecth_quote(tags)
+        self.search = tags
+
+        self.text, self.pagelen = wrapper.book_renderer(self.search, self.results, self.page)
 
     async def start_book(self, context):
-        self.msg = await context.send(gui1)
+        self.msg = await context.send(self.text)
+        if self.pagelen:
+            await self.msg.add_reaction(left_turn)
+            await self.msg.add_reaction(right_turn)
 
-    def flip(self, flip=1):
+    async def flip(self, flip=1):
         # 1 - next, -1 - previous
-        self.page += flip
-        self.msg.edit('this is page #%d.' % self.page)
+        if self.pagelen:
+            self.page += flip
+            if self.page > self.pagelen:
+                self.page = 1
+            if self.page < 1:
+                self.page = self.pagelen
+            self.text, self.pagelen = wrapper.book_renderer(self.search, self.results, self.page)
+            await self.msg.clear_reactions()
+            await self.msg.add_reaction(left_turn)
+            await self.msg.add_reaction(right_turn)
+
+            await self.msg.edit(content=self.text, delete_after=book_timeout)
+
+    async def close(self):
+        await self.msg.delete()
+
 
 @client.event
 async def on_ready():
@@ -30,52 +59,50 @@ async def on_ready():
 
 # how the person brings up the message
 @client.command(aliases=['quote'])
-async def book_spawn(ctx):
+async def book_spawn(ctx, *args):
     # needs to be turned into a book
-    book_obj = Book(ctx)
+    book_obj = Book(ctx, args)
     await book_obj.start_book(ctx)
-    current_books[book_obj.msg.id] = (book_obj)
+    current_books[book_obj.msg.id] = book_obj
+    last_100_books.append(book_obj.msg.id)
+    if len(last_100_books) > 100:
+        current_books[last_100_books[0]].close()
+        current_books[last_100_books[0]] = None
+        last_100_books.pop(0)
+
 
 @client.event
 async def on_reaction_add(reaction, channel):
     if reaction.emoji == '👍':
-      if 'Frozenbyte Developer' in [i.name for i in reaction.message.author.roles]:
-        print('emoji added')
-        print('"%s" quote by user "%s" with id @%s' % (
-        reaction.message.content, reaction.message.author, reaction.message.author.id))
-        wrapper.add_quote(reaction.message)
-        await channel.send('logged!')
-      else:
-        return
+        if FBdev in [i.name for i in reaction.message.author.roles]:
+            print('emoji added')
+            print('"%s" quote by user "%s" with id @%s' % (
+                reaction.message.content, reaction.message.author, reaction.message.author.id))
+            wrapper.add_quote(reaction.message)
+            await channel.send('logged!')
+        else:
+            return
 
-    elif reaction.emoji == 'de73a9116babdbce65076a42ac2ac2ba':
-        print('flip left')
-        current_books[-1].flip(-1)
+    if reaction.message.id in current_books.keys():
+        who_reacted = await reaction.users().flatten()
+        roles_who_reacted = []
+        for i in [i.roles for i in who_reacted]:
+            roles_who_reacted.extend(i)
+        if quotemod in [i.name for i in roles_who_reacted]:
+            if reaction.emoji == left_turn:
+                print('flip left')
+                await current_books[reaction.message.id].flip(-1)
+            elif reaction.emoji == right_turn:
+                print('flip right')
+                await current_books[reaction.message.id].flip(1)
 
-    elif reaction.emoji == '464b4ad3ec906581bdc288c42780b3c9':
-        print('flip right')
-        current_books[-1].flip(1)
-    else:
-        print(reaction.emoji)
-
-
-@client.command(aliases=['next'])
-async def next_page(ctx):
-  print('flip right')
-  current_books[-1].flip(1)
-
-@client.command(aliases=['before'])
-async def last_page(ctx):
-  print('flip left')
-  current_books[-1].flip(-1)
 
 @client.command(aliases=['searchs'])
 async def search(ctx, arg):
-  arg = str(arg)
-  args = arg.split()
-  for arg in args:
-    pass
-  
+    arg = str(arg)
+    args = arg.split()
+    for arg in args:
+        pass
 
 
 try:
--- wrapper.py
+++ wrapper.py
@@ -2,6 +2,7 @@ import sqlite3
 
 entry = sqlite3.connect("quotes.db")
 cur = entry.cursor()
+quotes_per_page = 10
 
 print('wrapper library imported.')
 
@@ -11,7 +12,47 @@ def add_quote(msg_obj):
     text, usr, msgID, usr_id = msg_obj.content, msg_obj.author, msg_obj.id, msg_obj.author.id
     dateUTC, jumplink = msg_obj.created_at, msg_obj.jump_url
 
-    cur.execute("""INSERT INTO quotes(content, author, msgID, authorID, date, jumplink) VALUES('%s', '%s', '%s', '%s', '%s', '%s')""" %
-                (text, usr, msgID, usr_id, dateUTC, jumplink))
+    cur.execute(
+        """INSERT INTO quotes(content, author, msgID, authorID, date, jumplink) VALUES('%s', '%s', '%s', '%s', '%s', '%s')""" %
+        (text.replace(r"'", r"`"), usr, msgID, usr_id, dateUTC, jumplink))
 
     entry.commit()
+
+
+def fecth_quote(tags=None):
+    # tags - list of strings1
+    if tags is None:
+        return cur.execute("""SELECT * FROM quotes""").fetchall()
+    tmp = []
+    for tag in tags:
+        tmp.append(r"content like '%{}%' ".format(tag))
+    request = ' OR '.join(tmp)
+    return cur.execute("""SELECT * FROM quotes WHERE """ + request).fetchall()
+
+
+def book_renderer(search=None, results=None, page=0):
+    if search is None:
+        search = ['all']
+    if results is None or len(results) == 0:
+        return "Sorry, no matches in my database. Maybe try again with different tags?", 0
+    else:
+        results = ["- '%s' by %s." % (i[0], i[1]) for i in results]
+
+    pages, tmp, k = [], [], 0
+    for i in results:
+        k += 1
+        if k == quotes_per_page:
+            k = 0
+            pages.append(tmp)
+            tmp = []
+        tmp.append(i)
+    if len(pages) == 0 and len(tmp):
+        pages = tmp
+    s = """your search: "%s"
+current page: %d / %d
+quotes:
+""" % ('; '.join(search), page, len(pages) + 1)
+    print("page", page, len(pages))
+    s += '\n'.join(pages[page - 1])
+
+    return s, len(pages) + 1
