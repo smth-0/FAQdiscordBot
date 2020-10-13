2d73d89f608320de50a43c4007e0a674d4cf8d6f
smth-0 <35414314+smth-0@users.noreply.github.com>
2020-10-13 10:32:46 +0600

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,23 +2,21 @@ import discord
 from discord.ext import commands
 
 
-key = 'Njc4MDI5MzM3OTk1Mzc4Njg4.Xk29uw.y-OzIKLsjlffAvgwzD0vFDzO_Z0' #<-- Bot run key
+key = '' #<-- Bot run key
 CF = "." #<--- The thing used at the start of every command
-client = commands.Bot(command_prefix=CF, case_insensitive=True) #<-- Just dont mess with
+client = commands.Bot(command_prefix=CF, case_insensitive=True)
 
 left_turn, right_turn = '⬅️', '➡️' #<-- Just dont mess with
 quote_emoji = '💾' #used to save quotes
 #
-FBdev = 'Frozenbyte Developer'  # quotable person
+FBdev = 'Admin'  # quotable person, can save quotes too
 quotemod = 'Moderator'  # role allowed to put quotes into DB
 quotes_per_page = 10
 
-db_name = "quotes.db" #<-- Also dont mess with
+db_name = "quotes.db"
 
 log_on_boot = 'successfully started'
 activity = 'with the database' # playing...
-
-#Im just gonna make main a bit cleaner -sweer
 
 faqhelp = """```diff
 commands list
@@ -29,11 +27,4 @@ commands list
 = ".get id <msgID>" - gives you jump link to the message for requested message ID. 
 Note: only quoted messages are supported. 
 = Adding 💾 emoji works as command to save the quote. 
-
-= Made by community members
-Lunar#1535
-sweer#6178
-Blue#8044
-= And a special thanks to
-Oobfiche#5954
-🐉Mr. Extinct#6090```""" #< --- ^^^ credits
+```
